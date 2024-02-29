9831ca50eedcb1d6f4b32ef76a6174dcff69f18d
Tranlevietanh <161743638+Tranlevietanh@users.noreply.github.com>
2024-02-29 23:56:31 +0700

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "person.cpp"
+#include "student.cpp"
+#include "lecturer.cpp"
+using namespace std; 
+
+int main () {
+	Person person1; 
+	cin >> person1; 
+	cout << person1;
+	
+	Student student1;
+	cin >> student1; 
+	cout << student1;
+	
+	Lecturer lecturer1; 
+	cin >> lecturer1; 
+	cout << lecturer1; 
+	}
